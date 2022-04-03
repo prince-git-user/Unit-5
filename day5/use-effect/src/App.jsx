5d5f3f6afb549b0edf8e4c0da233a949705d7781
prince-git-user <pk737490@gmail.com>
2022-04-03 23:46:16 +0530

    commited useeff todolist app

---
+++ day5/use-effect/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Todo } from './componets/Todo';
+
+function App() {
+  return (
+    <div className='main'>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
