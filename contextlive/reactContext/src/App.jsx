0bb1fa5323f23c6d4050691a5d815d3899ff05a2
prince-git-user <pk737490@gmail.com>
2022-05-03 12:38:04 +0530

    commit

--- contextlive/reactContext/src/App.jsx
+++ contextlive/reactContext/src/App.jsx
@@ -1,45 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
+import { useContext, useState } from "react";
+import "./App.css";
+import { Toogle } from "./components/Authentication/Toggleauth";
+import { Button } from "./components/Navebar/Button";
+import { Auth } from "./Context/AuthContext";
 function App() {
-  const [count, setCount] = useState(0)
+  const { boolen, token } = useContext(Auth);
 
+  // console.log(boolen);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <>
+      <div className="App">
+        <Button />
+        <Toogle />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
